433631a3f69d4dc2ccd9a597272d806c8493e7ac
apple <apple@apples-Mac-mini-2.local>
2021-02-02 11:00:33 +0530

    firebase installation

--- src/screens/cart.js
+++ src/screens/cart.js
@@ -122,6 +122,7 @@ console.log(cartData)
   };
    const paymentOption = (paymentType) => {
      orderPlace(paymentType).then((response) => {
+       console.log(response.message);
         setLoading(false)
         props.navigation.navigate('checkout', { paymentUrl: response.data.data.paymenturl })
       }).catch((error) => {
@@ -1291,7 +1292,7 @@ console.log(cartData)
         data={[
           { paymentType:1, icon:<Icons name="credit-card" color={"#D2D7F9"} size={25} />, key: 'KNET'},
           { paymentType:2,icon:<Icons name="credit-card" color={"#D2D7F9"}  size={25} />, key: 'VISA'},
-          { paymentType:2,icon:<Icons name="briefcase"  color={"#D2D7F9"} size={25}/>, key: 'Cash On Delivery'}, 
+          { paymentType:3,icon:<Icons name="briefcase"  color={"#D2D7F9"} size={25}/>, key: 'Cash On Delivery'}, 
         ]}
         renderItem={({item}) => <TouchableOpacity onPress={() => paymentOption(item.paymentType)} ><View style={styles.itemContainer} >{item.icon}<Text style={styles.item}>{item.key}</Text></View></TouchableOpacity>}
       />
